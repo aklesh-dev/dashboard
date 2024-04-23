988cef285325c68da10897ba5641bc58e4dc5ad4
aklesh yadav <arzunayavadu998@gmail.com>
2024-04-23 12:21:10 +0545

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,33 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span style={{ background: dotColor }}
         className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
       >
-        {icon}
       </span>
+      {icon}
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  },[]);
+
+  useEffect(() => {
+    if(screenSize <= 900){
+      setActiveMenu(false);
+    }else{
+      setActiveMenu(true);
+    }
+  },[screenSize]);
+
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
       <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
@@ -36,28 +55,28 @@ const Navbar = () => {
       <div className='flex'>
         <NavButton
           title="Cart"
-          // customFunc={() => handleClick('cart')}
+          customFunc={() => handleClick('cart')}
           color="blue"
           icon={<FiShoppingCart />}
         />
         <NavButton
           title="Chat"
           dotColor="#03C9D7"
-          // customFunc={() => handleClick('chat')}
+          customFunc={() => handleClick('chat')}
           color="blue"
           icon={<BsChatLeft />}
         />
         <NavButton
           title="Notification"
           dotColor="#03C9D7"
-          // customFunc={() => handleClick('notification')}
+          customFunc={() => handleClick('notification')}
           color="blue"
           icon={<RiNotification2Line />}
         />
         <TooltipComponent content="Profile" position="BottomCenter">
           <div 
             className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
-            // onClick={() => handleClick('userProfile')}
+            onClick={() => handleClick('userProfile')}
           >
             <img src={avatar} alt="avatar" className='rounded-full w-8 h-8' />
             <p>
@@ -67,6 +86,10 @@ const Navbar = () => {
             <MdKeyboardArrowDown className='text-gray-400 text-14' />
           </div>
         </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>    
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
-  // const activeMenu = true;
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
 
@@ -19,7 +24,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-3'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => { setActiveMenu(false) }}
+          <Link to="/" onClick={handleCloseSideBar}
             className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold
         tracking-tight dark:text-white text-slate-900'>
             <SiShopware /> <span>Shoppy</span>
@@ -45,7 +50,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => { }}
+                  onClick={handleCloseSideBar}
                   className={({ isActive }) =>
                     isActive ? activeLink : normalLink
                   }
