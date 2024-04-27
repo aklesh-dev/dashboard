a1660a412bdc4fed2a8ceaae9ff028f8c83d84a8
aklesh yadav <arzunayavadu998@gmail.com>
2024-04-27 15:19:18 +0545

    Sparkline added

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -29,9 +29,9 @@ const Ecommerce = () => {
 
         <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
           {earningData.map((item) => (
-            <div 
-            key={item.title}
-            className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'
+            <div
+              key={item.title}
+              className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'
             >
               <button
                 type='button'
@@ -55,25 +55,67 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-
+      {/* -----Revenue Section----- */}
       <div className='flex flex-wrap gap-10 justify-center'>
         <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
           <div className='flex justify-between'>
             <p className='font-semibold text-xl'>Revenue Updates</p>
             <div className='flex items-center gap-4'>
               <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
-                <span> <GoDotFill/> </span>
+                <span> <GoDotFill /> </span>
                 <span>Expense</span>
               </p>
               <p className='flex items-center gap-2 text-green-600 hover:drop-shadow-xl'>
-                <span> <GoDotFill/> </span>
+                <span> <GoDotFill /> </span>
                 <span>Budget</span>
               </p>
             </div>
           </div>
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>$98,562</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-sm ml-3 text-white bg-green-400'>27%</span>
+
+                </p>
+                <p className='text-gray-500 mt-2'>Budget</p>
+              </div>
+              <div className='mt-8 '>
+                <p>
+                  <span className='text-3xl font-semibold'>$48,562</span>                  
+                </p>
+                <p className='text-gray-500 mt-2'>Expense</p>
+              </div>
+
+              <div className='mt-5'>
+                <SparkLine
+                  currentColor='blue'
+                  id='line-sparkline'
+                  type='Line'
+                  height='80px'
+                  width='250px'
+                  data={SparklineAreaData}
+                  color='blue'
+                />
+              </div>
+
+              <div className='mt-5'>
+                <Button
+                  color='white'
+                  bgColor='blue'
+                  text='Download Report'
+                  borderRadius='10px'
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked />
+            </div>
+          </div>
         </div>
       </div>
-      
+
     </div>
   )
 }
