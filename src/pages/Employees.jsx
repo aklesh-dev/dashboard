cd0faeaf5d71b004572506440ba8258cf44460cb
aklesh yadav <arzunayavadu998@gmail.com>
2024-04-28 13:13:15 +0545

    added employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective,  Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesData,  employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+
+      <Header
+        title="Employees"
+        category="Page"
+      />
+      <GridComponent      
+      dataSource={employeesData}
+      allowPaging
+      allowSorting
+      toolbar={['Search']}
+      width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />
+          })}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+
   )
 }
 
