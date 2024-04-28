b54f4e198345532768886f3d0e5f4646a2adc756
aklesh yadav <arzunayavadu998@gmail.com>
2024-04-28 13:29:39 +0545

    added customer page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnDirective, ColumnsDirective, Page, Edit, Selection, Toolbar, Sort, Filter, Inject } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customer = () => {
   return (
-    <div>Customer</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header
+        title="Cumtomer"
+        category="Page"
+      />
+      <GridComponent      
+      dataSource={customersData}
+      allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      editSettings={{
+        allowDeleting: true,
+        allowEditing: true
+      }}
+      width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />
+          })}
+        </ColumnsDirective>
+        <Inject services={[ Page, Toolbar, Selection, Edit, Filter, Sort]} />
+      </GridComponent>
+    </div>
   )
 }
 
