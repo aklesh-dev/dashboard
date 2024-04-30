e4bcb48f10760359bd8c8a5d8c4a834d37dc195c
aklesh yadav <arzunayavadu998@gmail.com>
2024-04-30 17:37:07 +0545

    added editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, Toolbar, RichTextEditorComponent } from '@syncfusion/ej2-react-richtexteditor';
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:m-10 p-2 mt-24 md:p-10 rounded-3xl bg-white'>
+      <Header
+        category='App'
+        title='Editor'
+      />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+        <EditorData/>
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
