33eebbea75a78b5c7e38646a4705cdd64de628a8
aklesh yadav <arzunayavadu998@gmail.com>
2024-05-02 12:01:11 +0545

    added charts

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className='m-4 md:m-10 p-10 mt-24 bg-white rounded-3xl dark:bg-secondary-dark-bg'>
+      <Header category="Chart" title="Inflation Rate" />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
